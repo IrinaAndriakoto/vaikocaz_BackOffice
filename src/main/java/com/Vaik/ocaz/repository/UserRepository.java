44b4e2217347bf91bda6728ab0bd00b9018b1ba4
IrinaAndriakoto <119145568+IrinaAndriakoto@users.noreply.github.com>
2024-01-26 03:49:14 +0300

    stat nb of user by annonce

--- src/main/java/com/Vaik/ocaz/repository/UserRepository.java
+++ src/main/java/com/Vaik/ocaz/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Utilisateur, Long> {
     Utilisateur findByNom(String nom);
+    Long countByAnnoncesIsNotNull();
 }
